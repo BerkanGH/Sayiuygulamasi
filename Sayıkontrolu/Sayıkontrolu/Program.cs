5308773b95a3c7dbb5c58648f3834a4d123a7ead
BerkanGH <berkankeskin28@gmail.com>
2024-10-03 19:41:34 +0300

    patikaödev

---
+++ Sayıkontrolu/Sayıkontrolu/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Sayıuygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("merhaba bir sayı giriniz");
+            string sayi = Console.ReadLine();
+
+            int number = int.Parse(sayi);
+
+            if (number > 10)
+            {
+
+                Console.WriteLine("sayi 10 dan büyük");
+
+
+            }
+            else if (number < 10)
+            {
+
+                Console.WriteLine("sayı 10 dan küçük");
+
+
+            }
+
+            else {
+                Console.WriteLine("sayı 10 a eşit");
+            
+            
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("sayi 2 ye bölünüyor");
+
+
+            }
+
+            else {
+                Console.WriteLine("sayı ikiye bölünmüyor");   
+            
+            }
+        }
+    }
+
+}
